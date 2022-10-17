ba70f4de87c5197485941450c0589b970ab9a022
kingsodirxon <kingsodiroxon@gmail.com>
2022-10-17 17:36:13 +0500

    Начало

--- bot.py
+++ bot.py
@@ -1,5 +1,16 @@
-print("hello world")
+import pandas as pd
 
-a = "leg"
-b = "end"
-print(a + b)
+apiKey = '7Z5R5PTNGVS0GUSB'
+
+interval_var = '5min'
+symbol = 'ETH'
+
+path = 'https://www.alphavantage.co/query?function=CRYPTO_INTRADAY&symbol='+symbol + \
+    '&market=USD&interval='+interval_var+'&apikey=' + \
+    apiKey+'&datatype=csv&outputsize=full'
+
+df = pd.read_csv(path)
+
+df = df[::-1].reset_index()
+
+df = df.drop(['index'], axis=1)
